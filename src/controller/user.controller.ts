af1443bb9e6194244c6e7a4be4c6eed4cf80b00f
Maple <200214zef@gmail.com>
2023-12-20 19:44:10 +0800

    test

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -3,13 +3,14 @@ import { AppDataSource } from '@/app/database';
 import { User } from '../entity/Users';
 
 const userInfo = async (ctx: Context) => {
-  try {
-    const userRepository = AppDataSource.getRepository(User);
-    const res = await userRepository.find();
-    ctx.success('success', res);
-  } catch (error: any) {
-    ctx.emitError(error);
-  }
+  ctx.success('ok',{suername:'zf'})
+  // try {
+  //   const userRepository = AppDataSource.getRepository(User);
+  //   const res = await userRepository.find();
+  //   ctx.success('success', res);
+  // } catch (error: any) {
+  //   ctx.emitError(error);
+  // }
 };
 
 export { userInfo };
--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,21 @@ import { APP_PORT } from './app/config';
 // import { createDatabaseConnect } from './app/database';
 import { AppDataSource } from './app/database';
 
-AppDataSource.initialize()
-  .then(async () => {
-    app.listen(APP_PORT, async () => {
-      // await createDatabaseConnect();
-      console.log('服务器启动成功');
-      console.log(`API Docs:  
-        http://localhost:${APP_PORT}/docs/
-        http://${ip.address()}:${APP_PORT}/docs/`);
-    });
-  })
-  .catch((error) => console.log(error));
+// AppDataSource.initialize()
+//   .then(async () => {
+//     app.listen(APP_PORT, async () => {
+//       // await createDatabaseConnect();
+//       console.log('服务器启动成功');
+//       console.log(`API Docs:  
+//         http://localhost:${APP_PORT}/docs/
+//         http://${ip.address()}:${APP_PORT}/docs/`);
+//     });
+//   })
+//   .catch((error) => console.log(error));
+app.listen(APP_PORT, async () => {
+  // await createDatabaseConnect();
+  console.log('服务器启动成功');
+  console.log(`API Docs:  
+    http://localhost:${APP_PORT}/docs/
+    http://${ip.address()}:${APP_PORT}/docs/`);
+});
